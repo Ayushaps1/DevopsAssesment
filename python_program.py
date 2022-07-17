4689e55a145715e41884ced69a4b54f7ae4e676c
Ayush pratap singh <83778809+Ayushaps1@users.noreply.github.com>
2022-07-17 15:08:21 +0530

    Create python_program.py

---
+++ python_program.py
@@ -0,0 +1,8 @@
+list1 = [1, 19, 2, 15, 16, 4, 10, 10, 11]
+print("List after removing all the duplicate elements : ", list(set(list1)))
+
+list1.sort()
+print("Sorting list in ascending order : ", list1)
+
+list1.sort(reverse = True)
+print("Sorting list in descending order: ", list1)
